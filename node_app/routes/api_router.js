1d9bb12125bfa650b18487f5a6300f47766670a7
Meena Muthusubramanian <meena.muthusubramanian@vanderbilt.edu>
2022-04-11 18:23:33 +0530

    Updated backend

--- node_app/routes/api_router.js
+++ node_app/routes/api_router.js
@@ -22,7 +22,7 @@ routes.get('/:table/pagination', async (req, res) => {
         // const query1 = `SELECT COUNT(*) AS TOTALROWS FROM ${table};`;
         // const totalItems = await db.sequelize.query(query1, { type: Sequelize.QueryTypes.SELECT });
         
-        const query2 = `SELECT * FROM ${table} LIMIT ${start}, ${limit};`;
+        const query2 = `SELECT * FROM ${table} LIMIT ${start}, 1000;`;
         const results = await db.sequelize.query(query2, { type: Sequelize.QueryTypes.SELECT });
         const columns = Object.keys(results[0])
         const response = {
@@ -47,45 +47,94 @@ routes.get('/:table/pagination', async (req, res) => {
     }
 });
 
+routes.get('/:table/column/:column', async (req, res) => {
+    try {
+        const table = req.params.table;
+        const column = req.params.column;
+ 
+        const query = `SELECT DISTINCT ${column} FROM ${table};`;
+        const results = await db.sequelize.query(query, { type: Sequelize.QueryTypes.SELECT });
+        const values = [];
+        console.log(results)
+        results.forEach(data => {
+            values.push(Object.values(data)[0]);
+        } );
+        // const columns = Object.keys(results[0])
+        const response = {
+            "tableData": values,
+        };
+        return res.json(response);
+    
+    } catch (error) {
+        const response = {
+            "totalItems": 0,
+            "columns": ["db_result"],
+            "tableData": [{db_result: "No rows returned"}],
+            "message": "Error-> Cannot complete a paging request!",
+            "error": error.message
+        };
+        res.json(response);
+    }
+});
+
+
+
 routes.post('/:table/pagination', async (req, res) => {
     try {
         console.log(req.body)
         const table = req.params.table;
-        const data = req.body.data || {};
+        const filter = req.body.filter;
         const start = parseInt(req.query.start) || 0;
-        const limit = parseInt(req.query.limit) || 10;
-        console.log(data);
+        const client_limit = parseInt(req.query.limit) || 10;
+        const server_limit = 500;
+        console.log(filter);
         let totalRows = tableRows[table];
         let where_clause = '';
-        if (Object.keys(data).length !== 0) {
-            where_clause += 'WHERE '
-            let first = true;
-            Object.keys(data).forEach(column => {
-                if(data[column]) {
-                    if(!first) {
-                        where_clause += ' AND ';
-                    }
-                    where_clause += `${column} = "${data[column]}"`;
-                    first = false;
-                }
-            })
-            const query1 = `SELECT COUNT(*) AS TOTALROWS FROM ${table} ${where_clause};`;
+        if(filter) {
+            where_clause += ` WHERE ${filter}`
+            const query1 = `SELECT COUNT(*) AS TOTALROWS FROM ${table}${where_clause};`;
             console.log(query1)
             const totalItems = await db.sequelize.query(query1, { type: Sequelize.QueryTypes.SELECT });
             totalRows = totalItems[0].TOTALROWS;
-        } 
-        
-        
-        const query2 = `SELECT * FROM ${table} ${where_clause} LIMIT ${start}, ${limit};`;
+        }
+        // if (Object.keys(filter).length !== 0) {
+        //     where_clause += ' WHERE '
+        //     let first = true;
+        //     Object.keys(filter).forEach(column => {
+        //         if(filter[column]) {
+        //             if(!first) {
+        //                 where_clause += ' AND ';
+        //             }
+        //             where_clause += `${column} = "${filter[column]}"`;
+        //             first = false;
+        //         }
+        //     })
+            
+        //     const query1 = `SELECT COUNT(*) AS TOTALROWS FROM ${table}${where_clause};`;
+        //     console.log(query1)
+        //     const totalItems = await db.sequelize.query(query1, { type: Sequelize.QueryTypes.SELECT });
+        //     totalRows = totalItems[0].TOTALROWS;
+        // } 
+        where_clause += ' '
+        const query2 = `SELECT * FROM ${table}${where_clause}LIMIT ${start}, ${server_limit};`;
         const results = await db.sequelize.query(query2, { type: Sequelize.QueryTypes.SELECT });
+        if(!results.length){
+            return res.status(404).json({
+                message: "No rows returned for the given filter",
+                error_msg: "No rows returned"
+              });
+        }
+        console.log(results);
         const columns = Object.keys(results[0])
         const response = {
             table,
             columns,
             totalRows,
             "tableData": results,
+            filter,
             start,
-            limit,
+            client_limit,
+            server_limit,
         };
         return res.json(response);
     }  catch(err) {
